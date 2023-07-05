37175ae33a237e930dbeb3c91e194c2ee9b6be1d
Felipe Hidequel <92414576+felipehidequel@users.noreply.github.com>
2023-07-05 19:17:34 +0000

    added questao1.c

---
+++ questao1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+
+    int tamanho;
+    printf("Informe o tamanho do vetor\n");
+    scanf("%d", &tamanho);
+
+    int * vetor = (int*) malloc(tamanho * sizeof(int*));
+    if(vetor==NULL){
+        printf(" Memory allocation error!");
+        exit(1);
+    }
+    
+    //Recebe elementos inseridos pelo user
+    printf("Informe os valores para o seu vetor de tamanho %i\n", tamanho );
+    for (int index = 0; index < tamanho; index++){
+        scanf("%d", &vetor[index]);
+    }
+
+    for (int index = 0; index > tamanho; index--){
+        printf("%i\n", vetor[index]);
+    }
+
+    return 0;
+}
